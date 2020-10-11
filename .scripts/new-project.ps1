67a7d2e3ede8c3923552979ba732996c12ca2ace
Javanie Campbell <javanie.mcampbell@outlook.com>
2020-10-10 22:38:38 -0500

    Added the script for generating the project structure.

---
+++ .scripts/new-project.ps1
@@ -0,0 +1,20 @@
+# [CmdletBinding()]
+# param (
+#     [Parameter()]
+#     [string]
+#     $ProjectName
+# )
+
+$currentPath = Split-Path -Parent $MyInvocation.MyCommand.Definition 
+
+$folders = @(".docker", ".infrastructure", ".k8s", ".scripts", ".tools")
+Write-Host "Creating directories.."
+foreach ($folder in $folders) {
+    # Write-Host "Creating directories at [$folder]..."
+    mkdir "$currentPath\$folder"
+    New-Item "$currentPath\$folder\.gitkeep"
+    # Write-Host  "[$folder] was created successfully"
+}
+Write-Host  "Project was created successfully"
+
+Write-Host "Start hacking to launch 🚀"
